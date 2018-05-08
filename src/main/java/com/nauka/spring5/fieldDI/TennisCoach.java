e9f3623fcf7563c205ab315d35b3121e80147a38
Igor Szczerbicki <pl4cek@outlook.com>
2018-05-08 08:49:00 +0200

    DI - wstrzykiwanie zaleznosci do pol
    AdnotacjaApp.java
    - uruchamia program
    
    TennisCoach.java
    - adnotacja @Autowired przy polu powoduje automatyczne wstrzykniecie zaleznosci

--- src/main/java/com/nauka/spring5/fieldDI/TennisCoach.java
+++ src/main/java/com/nauka/spring5/fieldDI/TennisCoach.java
@@ -1,10 +1,12 @@
 package com.nauka.spring5.fieldDI;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class TennisCoach implements Coach {
 
+	@Autowired
 	private FortuneService fortuneService;
 	
 	public TennisCoach() {}
