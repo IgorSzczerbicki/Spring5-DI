f32f6840cc06607cc9439eaa14b5f961b605ee7d
Igor Szczerbicki <pl4cek@outlook.com>
2018-05-09 08:39:03 +0200

    @scope - definiowanie zasięgu Bean'ów
    FootballCoach.java
    - adnotacja @ScopeApp("prototype"), oznacza, ze kazde pobranie Beanu z kontekstu zwroci nowy obiekt

--- src/main/java/com/nauka/spring5/scope/ScopeApp.java
+++ src/main/java/com/nauka/spring5/scope/ScopeApp.java
@@ -12,8 +12,14 @@ public class ScopeApp {
 		Coach theCoach = context.getBean("tennisCoach", Coach.class);
 		Coach theCoach2 = context.getBean("tennisCoach", Coach.class);
 		
+		//zaladowanie Bean'ów typu Prototype z kontenera
+		Coach theFootballCoach = context.getBean("footballCoach", Coach.class);
+		Coach theFootballCoach2 = context.getBean("footballCoach", Coach.class);
+		
 		//sprawdzenie, czy te same
 		System.out.println("Signleton, te same obiekty? " + (theCoach == theCoach2));
+		
+		System.out.println("Prototype, te same obiekty? " + (theFootballCoach == theFootballCoach2));
 		
 		//zamkniecie kotekstu
 		context.close();
